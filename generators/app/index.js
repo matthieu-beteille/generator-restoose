a1b4709f7fff30cbbdc6ab57e803ba640089dfda
matthieu-beteille <matthieu.beteille@gmail.com>
2014-11-06 21:59:03 +0100

    'generator'

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,17 +3,19 @@ var ncp = require('ncp').ncp;
 
 module.exports = generators.Base.extend({
   method1: function () {
-      this.log('Generating restoose project');
+      this.log('Generating your restoose project');
+      this.log('1/3 Fetching restoose from github');
       this.remote('matthieu-beteille', 'restoose', function(err, remote, files) {
         if(err){
           this.log(err);
         } else {
           var self= this;
+          this.log('2/3 Copying files ');
           ncp(remote.src._base,this.destinationRoot(), function (err) {
             if (err) {
-              return console.error(err);
+              return this.error(err);
             }
-            console.log('File copied!');
+            self.log('3/3 Installing dependencies ... ');
             self.npmInstall();
           });
         }
