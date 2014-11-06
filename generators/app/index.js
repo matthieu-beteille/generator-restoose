4cf6513f9b01438bb792230aab8fdf7286646e30
matthieu-beteille <matthieu.beteille@gmail.com>
2014-11-06 22:35:16 +0100

    adding project name argument

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,14 @@
 var generators = require('yeoman-generator');
 var ncp = require('ncp').ncp;
+var path = require('path');
 
 module.exports = generators.Base.extend({
   method1: function () {
+    try{
+      this.argument('name', {required: true});
+      var appName = this['name'];
+      this.mkdir(appName);
+      this.destinationRoot(path.join(this.destinationRoot(), appName));
       this.log('Generating your restoose project');
       this.log('1/3 Fetching restoose from github');
       this.remote('matthieu-beteille', 'restoose', function(err, remote, files) {
@@ -20,5 +26,10 @@ module.exports = generators.Base.extend({
           });
         }
       }.bind(this));
+    } catch(err){
+      this.log('Error while creating your project');
+      this.log(err);
+    }
+
   }
 });
